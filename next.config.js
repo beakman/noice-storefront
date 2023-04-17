cebbc8becc9dcb0ac29ac83bc2ad727263fa5530
turboplebeian <127253739+turboplebeian@users.noreply.github.com>
2023-04-17 23:36:54 +0200

    Recover test image domain

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["minio.noice.store", "localhost"],
+    domains: ["minio.noice.store", "medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
   },
 })
 
