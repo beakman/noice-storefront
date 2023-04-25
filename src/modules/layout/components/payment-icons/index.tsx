c641c89d0e83b5267e78641c4908c9cce0b429bf
beakman <beakman@users.noreply.github.com>
2023-04-26 01:50:09 +0200

    Actualizar index.tsx

--- src/modules/layout/components/payment-icons/index.tsx
+++ src/modules/layout/components/payment-icons/index.tsx
@@ -5,9 +5,10 @@ const store = require("../../../../../store.config.json")
 const PaymentIcons = () => {
   return (
     <div className="flex items-center justify-between gap-x-3">
-      {Object.keys(store.payment).map((method) => {
+      {Object.keys(store.payment).map((method, index) => {
         return (
           <Image
+            key={index}
             alt={method}
             src={store.payment[method]}
             width="36"
