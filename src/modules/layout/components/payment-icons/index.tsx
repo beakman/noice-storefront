4fc469dbece50a95d84b9804d847b09acd994207
Turbo <turboplebeian@gmail.com>
2023-04-26 01:32:01 +0200

    Add payment icons

---
+++ src/modules/layout/components/payment-icons/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+
+const store = require("../../../../../store.config.json")
+
+const PaymentIcons = () => {
+  return (
+    <div className="flex items-center justify-between gap-x-3">
+      {Object.keys(store.payment).map((method) => {
+        return (
+          <Image
+            alt={method}
+            src={store.payment[method]}
+            width="36"
+            height="22"
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default PaymentIcons
