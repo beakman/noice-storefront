5eaf6ac0ee2c3c41484278dc4495970a708c7326
Turbo <turboplebeian@gmail.com>
2023-04-26 01:34:14 +0200

    Add more information to default footer component

--- src/modules/layout/components/footer-nav/index.tsx
+++ src/modules/layout/components/footer-nav/index.tsx
@@ -2,6 +2,8 @@ import clsx from "clsx"
 import { useCollections } from "medusa-react"
 import Link from "next/link"
 import CountrySelect from "../country-select"
+import PaymentIcons from "../payment-icons"
+import SocialIcons from "../social-icons"
 
 const store = require("../../../../../store.config.json")
 
@@ -11,30 +13,14 @@ const FooterNav = () => {
   return (
     <div className="content-container flex flex-col gap-y-8 pt-16 pb-8">
       <div className="flex flex-col gap-y-6 xsmall:flex-row items-start justify-between">
-        <div>
-          <Link href="/" className="text-xl-semi uppercase">
-            {store.site.name}
-          </Link>
-        </div>
-        <div className="text-small-regular grid grid-cols-2 gap-x-16">
-          <div className="flex flex-col gap-y-2">
-            <span className="text-base-semi">Collections</span>
-            <ul
-              className={clsx("grid grid-cols-1 gap-y-2", {
-                "grid-cols-2": (collections?.length || 0) > 4,
-              })}
-            >
-              {collections?.map((c) => (
-                <li key={c.id}>
-                  <Link href={`/collections/${c.id}`}>
-                    {c.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+        <div className="text-small-regular grid grid-cols-1 md:grid-cols-3 gap-y-8 md:gap-x-16">
+          <div>
+            <Link href="/" className="text-xl-semi uppercase">
+              {store.site.name}
+            </Link>
           </div>
           <div className="flex flex-col gap-y-2">
-            <span className="text-base-semi">Medusa</span>
+            <span className="text-base-semi">Quick links</span>
             <ul className="grid grid-cols-1 gap-y-2">
               <li>
                 <a
@@ -65,18 +51,31 @@ const FooterNav = () => {
               </li>
             </ul>
           </div>
+          <div className="flex flex-col gap-y-2">
+            <span className="text-base-semi">Our mission</span>
+            <p>
+              Provide unique, edgy, and fashion-forward clothing that empowers
+              our customers to express themselves and stand out from the crowd.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col-reverse gap-y-4 justify-center xsmall:items-center xsmall:flex-row xsmall:items-end xsmall:justify-between">
-        <span className="text-xsmall-regular text-gray-500">
-          © Copyright 2022 ACME
-        </span>
-        <div className="min-w-[316px] flex xsmall:justify-end">
+      <div className="flex my-5 justify-center items-center">
+        <SocialIcons />
+      </div>
+      <div className="flex flex-col gap-y-4 justify-center xsmall:items-start xsmall:flex-row xsmall:justify-between">
+        <div className="min-w-[316px] flex justify-center md:justify-start">
           <CountrySelect />
+        </div>
+        <div className="flex flex-col items-center md:items-end gap-y-4">
+          <PaymentIcons />
+          <div className="text-xsmall-regular text-gray-500">
+            © Copyright 2023 ACME
+          </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default FooterNav
